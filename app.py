ee7ac9cec111de8a14e277f4f09fd63438dfcda6
Inam032 <alihaiderjavaid77@gmail.com>
2025-10-31 14:39:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.set_page_config(page_title="Automobile Spare Parts", page_icon="🚗", layout="wide")
 
 # ---- TITLE ----
-st.title("🚗 Automobile Spare Parts Management App")
+st.title("🚗 Automotive Spare Parts")
 st.markdown("Easily manage your spare parts inventory, suppliers, and sales records.")
 
 # ---- INITIAL DATA ----
