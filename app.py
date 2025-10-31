625a5161122ea2939ffe4e5b8685e4aa3a859cb1
Inam032 <alihaiderjavaid77@gmail.com>
2025-10-31 15:39:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ if page == "Add New Part":
     if submitted:
         if part_Number and part_name:
             new_part = {
-                "Part Number": part_number,
+                "Part ID": part_ID,
                 "Part Name": part_name,
                 "Category": category,
                 "Quantity": quantity,
